36ae3a3ac8b93a662e345eb5137b6b26a4bbe8ee
vevel247 <ravelcouttyplus@gmail.com>
2025-04-22 04:58:10 +0000

    Criação da entrada de dados manual de duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,85 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//Variaveis para armazenamento de dados das cartas
+char estado[10], codigoCidade[20], uf[2]; //Armazenamento de strings
+int populacao, poontosTuristicos; //Armazenamento de numeros inteiros
+float area, pib; //Armazenamento de numeros flotantes
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+//Registro da primeira carta
+void cartaA(){
+    printf("Digite o nome do estado: ");
+    scanf("%s", estado);
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigoCidade);
+    printf("Digite a sigla do estado: ");
+    scanf("%s", uf);
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao);
+    printf("Digite a area: ");
+    scanf("%f", &area);
+    printf("Digite o pib: ");
+    scanf("%f", &pib);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &poontosTuristicos);
+}
+//Registro da segunda carta
+void cartaB(){
+
+    printf("Digite o nome do estado: ");
+    scanf("%s", estado);
+    printf("Digite o codigo da carta: ");
+    scanf("%s", codigoCidade);
+    printf("Digite a sigla do estado: ");
+    scanf("%s", uf);
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao);
+    printf("Digite a area: ");
+    scanf("%f", &area);
+    printf("Digite o pib: ");
+    scanf("%f", &pib);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &poontosTuristicos);
+}
+
+//* Exibir cartas **/
+void exibirCartas(){
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", codigoCidade);
+    printf("Sigla do estado: %s\n", uf);
+    printf("Populacao: %d\n", populacao);
+    printf("Area: %.2f\n", area);
+    printf("Pib: %.2f\n", pib);
+    printf("Pontos turisticos: %d\n", poontosTuristicos);
+}
+//* Exibir cartas A **/
+void exibirCartasA(){
+    printf("*** Carta *** \n Seu codigo de cidade e: %s\n", codigoCidade);
+
+    exibirCartas(); //Chamado da função exibirCartas para carta A
+}
+//* Exibir cartas B **/
+void exibirCartasB(){
+    printf("*** Carta *** \n Seu codigo de cidade e: %s\n", codigoCidade);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    exibirCartas(); //Chamado da função exibirCartas para carta B
+}
+//Organizar chamado de funções
+int main(){
+
+    cartaA(); //Registro da primeira carta
+    printf("*** A carta foi registrada com sucesso *** \n");
+    printf("\n"); //Pular linha
+    cartaB(); //Registro da segunda carta
+    printf("*** A carta foi registrada com sucesso *** \n");
+    printf("\n"); //Pular linha
+
+    //Exibir todas as cartas
+    printf("\n"); //Pular linha
+    exibirCartasA(); //Exibir a primeira carta
+    printf("\n"); //Pular linha
+    exibirCartasB(); //Exibir a segunda carta
+
 
     return 0;
+
 }
